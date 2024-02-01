a36e2e73c9444093a591b33c63dc53ff63d32b95
K.M. Jones-Eastland <kmje405@gmail.com>
2024-02-01 15:39:15 -0700

    Add Redux and Redux Thunk dependencies, and wrap App component with Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App.tsx';
+import { store } from './store'; // Ensure this is the correct path to your store
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}> {/* Wrap App with Provider */}
+      <App />
+    </Provider>
   </React.StrictMode>,
-)
+);
