662c34a55fd32bec40c1d81b2bdd90869cdc8c34
K.M. Jones-Eastland <kmje405@gmail.com>
2024-02-01 15:43:29 -0700

    Refactor import statement in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App.tsx';
-import { store } from './store'; // Ensure this is the correct path to your store
+import { store } from './store'; 
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}> {/* Wrap App with Provider */}
+    <Provider store={store}> 
       <App />
     </Provider>
   </React.StrictMode>,
