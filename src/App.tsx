ccd94a62b6f3f552526e94ba84bded5836ed60db
Abigail Dawson <abigaildawson.dev@gmail.com>
2024-02-10 12:35:03 -0500

    Adds basic UserProfilePage layout and beginning of edit functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import LoginPage from './features/auth/LoginPage'
+import UserProfilePage from './features/UserProfilePage/components/UserProfilePage'
 
 function App() {
   return (
     <>
      <LoginPage />
+     <UserProfilePage />
     </>
   )
 }
