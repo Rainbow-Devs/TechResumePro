c4a60d9c38106636ab91b47f15a60a6d6f6ad44c
K.M. Jones-Eastland <kmje405@gmail.com>
2024-02-11 16:13:51 -0700

    Add Container component to LandingPage

---
+++ src/features/LandingPage/components/Container.tsx
@@ -0,0 +1,11 @@
+import  { StyledContainer }  from "./styles/Container.styled";
+
+const Container = () => {
+  return (
+    <StyledContainer>
+      Hello World
+    </StyledContainer>
+  );
+};
+
+export default Container;
---
+++ src/features/LandingPage/components/styles/Container.styled.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+//example of a container component styles
+ export const StyledContainer = styled.div`
+	css goes here
+	width: 1000;
+    background-color: #f5f5f5;
+	max-width: 100%
+	padding: 0 20px;
+`
